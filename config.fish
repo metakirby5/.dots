fa92d4dbfcdb11fbcb563c68444c2e76aec57098
Ethan Chan <metakirby5@gmail.com>
2015-03-30 04:56:48 -0700

    port over myshellrc to fish

---
+++ config.fish
@@ -0,0 +1,50 @@
+function fish_greeting
+  # greeting here
+end
+
+# Environment variables
+set -e VISUAL vim
+set -e EDITOR $VISUAL
+set -e CLASSPATH '*':'.'
+
+# Aliases
+function flatten-dir
+  find . -mindepth 2 -type f -exec mv -t . -i '{}' +
+end
+
+## Git
+function ghp-publish
+  set -l cur_branch (git rev-parse --abbrev-ref HEAD ^/dev/null)
+  git checkout master
+  and git pull
+  and git checkout gh-pages
+  and git pull
+  and git merge master --no-edit
+  and git push
+  and git checkout $cur_branch
+end
+
+function clean-branches
+  git checkout master
+  and git branch --merged master | grep -v '\* master$' | xargs -n 1 git branch -d
+end
+
+## Program-specific
+function clean-chrome
+  killall chrome
+  and rm ~/.config/google-chrome/Default/Web\ Data
+end
+
+# Functions
+## Compile and run Java program
+function javar
+    if [ ! $argv[1] ]
+        echo 'Usage: javar <class name>'
+        echo 'Example for a file named "Test.java":'
+        echo '    $ javar Test'
+        return
+    end
+    javac $argv[1].java
+    java $argv[1]
+end
+
---
+++ prompt.fish
@@ -0,0 +1,68 @@
+# Prompt functions
+if not set -q -g __fish_mk5_prompt_functions_defined
+  set -g __fish_mk5_prompt_functions_defined
+
+  function __fish_mk5_git_pwd
+    # Get git base directory
+    set -l gitbase (git rev-parse --show-toplevel ^/dev/null)
+    if [ $gitbase ]
+      # Format for sed
+      set -l sed_gitbase (echo $gitbase | sed 's/\//\\\\\//g')
+      # Strip git base directory
+      echo -s (basename $gitbase) (pwd | sed "s/$sed_gitbase//g")
+    else
+      # Replace home with ~
+      if [ $USER = 'root' ]
+        echo (pwd) | sed "s/^\/root/~/"
+      else
+        echo (pwd) | sed "s/^\/home\/$USER/~/"
+      end
+    end
+  end
+
+  function __fish_mk5_git_branch_name
+    echo (git rev-parse --abbrev-ref HEAD ^/dev/null)
+  end
+
+  function __fish_mk5_git_is_dirty
+    echo (git status -s --ignore-submodules=dirty ^/dev/null)
+  end
+
+end
+
+function fish_prompt
+  # Grab status code first
+  set -l last_status $status
+
+  if [ $last_status = 0 ]
+    set chevcolor (set_color -o green)
+  else
+    set chevcolor (set_color -o red)
+  end
+
+  if [ $USER = 'root' ]
+    set chev '»'
+  else
+    set chev '›'
+  end
+
+  echo -n -s (set_color green) (__fish_mk5_git_pwd) ' ' \
+             $chevcolor $chev (set_color normal) ' '
+end
+
+function fish_right_prompt
+  set -l yellow (set_color -o yellow)
+  set -l gray (set_color -o 555)
+  set -l normal (set_color normal)
+
+  if [ (__fish_mk5_git_branch_name) ]
+    set git_info $gray(__fish_mk5_git_branch_name)
+
+    if [ (__fish_mk5_git_is_dirty) ]
+      set -l dirty "$yellow✗"
+      set git_info "$dirty $git_info"
+    end
+  end
+
+  echo -n -s $git_info $normal
+end
