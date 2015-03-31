b4f97debcb99d13723e17b7e9ce0d9d05cc793a4
Ethan Chan <metakirby5@gmail.com>
2015-03-31 01:28:19 -0700

    outgoing/incoming bugfix

--- prompt.fish
+++ prompt.fish
@@ -30,11 +30,11 @@ function __mk5_git_dirty
 end
 
 function __mk5_git_outgoing
-  echo (git log origin/HEAD..HEAD | grep '^commit' | wc -l)
+  echo (git log origin/HEAD..HEAD ^/dev/null | grep '^commit' | wc -l)
 end
 
 function __mk5_git_incoming
-  echo (git log HEAD..origin/HEAD | grep '^commit' | wc -l)
+  echo (git log HEAD..origin/HEAD ^/dev/null | grep '^commit' | wc -l)
 end
 
 function fish_prompt
