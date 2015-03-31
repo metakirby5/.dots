9c5d70985f0ee9fe29c42e142b8e96cd1ba7f7c6
Ethan Chan <metakirby5@gmail.com>
2015-03-30 19:14:08 -0700

    namespacing and colors

--- prompt.sh
+++ prompt.sh
@@ -1,23 +1,27 @@
-# Please set the following colors before sourcing this file:
-#   $__bash_mk5_color_normal
-#   $__bash_mk5_color_status_ok
-#   $__bash_mk5_color_status_bad
+# Please set the following variables before sourcing this file:
+#   $__mk5_color_normal
+#   $__mk5_color_branch
+#   $__mk5_color_dirty
+#   $__mk5_color_status_ok
+#   $__mk5_color_status_bad
 
-function __bash_mk5_git_dirty {
+function __mk5_git_dirty {
   local dirtymark='\xc2\xb1\x0a'
   if [[ "$(git status -s --ignore-submodules=dirty 2>/dev/null)" ]]; then
-    printf " $dirtymark"
+    printf " $__mk5_color_dirty$dirtymark$__mk5_color_normal"
   fi
 }
 
-function __bash_mk5_git_branch {
+function __mk5_git_branch {
   local branch="$(git rev-parse --abbrev-ref HEAD 2>/dev/null)"
   if [[ $branch ]]; then
-    printf "($branch$(__bash_mk5_git_dirty)) "
+    printf "$__mk5_color_branch\
+($branch$(__mk5_git_dirty)\
+$__mk5_color_branch)$__mk5_color_normal "
   fi
 }
 
-function __bash_mk5_git_pwd {
+function __mk5_git_pwd {
   # Get git base directory
   local gitbase=$(git rev-parse --show-toplevel 2>/dev/null)
   if [[ $gitbase ]]; then
@@ -34,15 +38,15 @@ function __bash_mk5_git_pwd {
 }
 
 # arg 1 is last status code
-function __bash_mk5_chevron {
+function __mk5_chevron {
   local prompt_char='\xe2\x9d\xb1\x0a'
   local prompt
   local color
 
   if [[ $1 == 0 ]]; then
-    color="$__bash_mk5_color_status_ok"
+    color="$__mk5_color_status_ok"
   else
-    color="$__bash_mk5_color_status_bad"
+    color="$__mk5_color_status_bad"
   fi
 
   if [[ $USER == 'root' ]]; then
@@ -51,14 +55,14 @@ function __bash_mk5_chevron {
     prompt="$prompt_char"
   fi
 
-  printf "$color$prompt$__bash_mk5_color_normal"
+  printf "$color$prompt$__mk5_color_normal"
 }
 
-function __bash_mk5_set_prompt {
+function __mk5_set_prompt {
   local last_status=$?
-  export PS1='$(__bash_mk5_git_branch)\
-$(__bash_mk5_git_pwd) \
-$(__bash_mk5_chevron last_status) '
+  export PS1='$(__mk5_git_branch)\
+$(__mk5_git_pwd) \
+$(__mk5_chevron last_status) '
 }
 
-__bash_mk5_set_prompt
+__mk5_set_prompt
