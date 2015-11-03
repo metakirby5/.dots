24270b54141fcde4238feeadea6483fce2a1bc66
Ethan Chan <metakirby5@gmail.com>
2015-11-03 23:40:12 +0900

    properly quote...

--- shell/config.sh
+++ shell/config.sh
@@ -20,23 +20,23 @@ alias ls='ls -h --color=auto'
 
 # Detect filetype and extract
 extract () {
-  if [ -f $1 ] ; then
-    case $1 in
-      *.tar.bz2) tar xvjf $1 ;;
-      *.tar.gz) tar xvzf $1 ;;
-      *.bz2) bunzip2 $1 ;;
-      *.rar) rar x $1 ;;
-      *.gz) gunzip $1 ;;
-      *.tar) tar xvf $1 ;;
-      *.tbz2) tar xvjf $1 ;;
-      *.tgz) tar xvzf $1 ;;
-      *.zip) unzip $1 ;;
-      *.Z) uncompress $1 ;;
-      *.7z) 7z x $1 ;;
-      *) echo "don't know how to extract '$1'..." ;;
+  if [ -f "$1" ] ; then
+    case "$1" in
+      *.tar.bz2) tar xvjf "$1" ;;
+      *.tar.gz) tar xvzf "$1" ;;
+      *.bz2) bunzip2 "$1" ;;
+      *.rar) rar x "$1" ;;
+      *.gz) gunzip "$1" ;;
+      *.tar) tar xvf "$1" ;;
+      *.tbz2) tar xvjf "$1" ;;
+      *.tgz) tar xvzf "$1" ;;
+      *.zip) unzip "$1" ;;
+      *.Z) uncompress "$1" ;;
+      *.7z) 7z x "$1" ;;
+      *) echo "don't know how to extract \"$1\"..." ;;
     esac
   else
-    echo "'$1' is not a valid file!"
+    echo "\"$1\" is not a valid file!"
   fi
 }
 
