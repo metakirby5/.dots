faa648a525a6eb45fdf4f79b7926770219c7475c
Ethan Chan <metakirby5@gmail.com>
2016-01-05 19:58:09 -0800

    bold whites in oomox

--- home/.config/whizkers/templates/oomox/colors/whizkers.sh
+++ home/.config/whizkers/templates/oomox/colors/whizkers.sh
@@ -2,10 +2,10 @@
      BG={{ bgc_nohash }}
      FG={{ fgc_nohash }}
  SEL_BG={{ n_primary_nohash }}
- SEL_FG={{ n_white_nohash }}
+ SEL_FG={{ b_white_nohash }}
  TXT_BG={{ bgc_nohash }}
  TXT_FG={{ fgc_nohash }}
 MENU_BG={{ bgc_nohash }}
 MENU_FG={{ fgc_nohash }}
  BTN_BG={{ n_primary_nohash }}
- BTN_FG={{ n_white_nohash }}
+ BTN_FG={{ b_white_nohash }}
