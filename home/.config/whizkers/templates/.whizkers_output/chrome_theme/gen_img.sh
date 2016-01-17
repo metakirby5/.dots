d88a50f81898babdb2b1a4e61c30cb52e47a8db3
Ethan Chan <metakirby5@gmail.com>
2016-01-16 20:12:46 -0800

    fix gen_img

--- home/.config/whizkers/templates/.whizkers_output/chrome_theme/gen_img.sh
+++ home/.config/whizkers/templates/.whizkers_output/chrome_theme/gen_img.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+images=~/.whizkers_output/chrome_theme/images
 
-mkdir -p images
-convert -size 30x256 xc:{{ bgc }} images/theme_frame.png
-convert -size 30x256 xc:{{ fgc }} images/theme_toolbar.png
+mkdir -p "$images"
+convert -size 30x256 xc:{{ bgc }} "$images/theme_frame.png"
+convert -size 30x256 xc:{{ fgc }} "$images/theme_toolbar.png"
