ce68dff9c4c90d80c18858d6ea303cabdac8335a
Ethan Chan <metakirby5@gmail.com>
2016-01-19 22:29:23 -0800

    fix incognito, inactive, change colors to match i3 bars

--- home/.config/whizkers/templates/.whizkers_output/chrome_theme/gen_img.sh
+++ home/.config/whizkers/templates/.whizkers_output/chrome_theme/gen_img.sh
@@ -2,5 +2,7 @@
 images=~/.whizkers_output/chrome_theme/images
 
 mkdir -p "$images"
-convert -size 30x256 xc:{{ bgc }} "$images/theme_frame.png"
-convert -size 30x256 xc:{{ fgc }} "$images/theme_toolbar.png"
+convert -size 30x256 xc:{{ n_primary }} "$images/theme_frame.png"
+convert -size 30x256 xc:{{ n_secondary }} "$images/theme_frame_inactive.png"
+convert -size 30x256 xc:{{ n_white }} "$images/theme_tab_background.png"
+convert -size 30x256 xc:{{ b_white }} "$images/theme_toolbar.png"
