ed2f6e20729dc31c2fe95a3f159e0ad1d44eee87
Ethan Chan <metakirby5@gmail.com>
2016-01-21 22:31:08 -0800

    add transfer

--- shell/config.sh
+++ shell/config.sh
@@ -33,6 +33,27 @@ up2ixio() {
   command curl -sfF 'f:1=<-' ix.io | xargs echo -n
 }
 
+transfer() {
+  if [ $# -eq 0 ]; then
+    echo "No arguments specified. Usage:
+    transfer [file]
+    cat [file] | transfer [url]"
+    return 1
+  fi
+
+  tmpfile="$(mktemp -t transferXXX)"
+
+  if tty -s; then
+    basefile="$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g')"
+    curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile
+  else
+    curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile
+  fi
+
+  cat $tmpfile
+  rm -f $tmpfile
+}
+
 # Detect filetype and extract
 extract () {
   if [ -f "$1" ] ; then
