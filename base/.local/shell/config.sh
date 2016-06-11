3b334e169fa87f0e6cf769cf9ed853f315a79a93
Ethan Chan <metakirby5@gmail.com>
2016-06-11 02:30:03 -0700

    history stuff

--- base/.local/shell/config.sh
+++ base/.local/shell/config.sh
@@ -7,6 +7,15 @@ export LS_COLORS='di=01;34:'
 # Don't give literal * unless we ask for it
 shopt -s nullglob
 
+# Big history
+export HISTCONTROL=ignoredups:erasedups  # no duplicate entries
+export HISTSIZE=100000                   # big big history
+export HISTFILESIZE=100000               # big big history
+shopt -s histappend                      # append to history, don't overwrite it
+
+# Save and reload the history after each command finishes
+export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+
 # http://stackoverflow.com/a/246128
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for f in $dir/{aliases,functions}/* $dir/prompt.sh; do
