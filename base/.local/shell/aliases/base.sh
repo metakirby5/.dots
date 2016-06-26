389e161af586a47a2738eb0ca78ef4447001d77c
Ethan Chan <metakirby5@gmail.com>
2016-06-26 02:18:38 -0700

    add quick update-conf alias, notify funcs

--- base/.local/shell/aliases/base.sh
+++ base/.local/shell/aliases/base.sh
@@ -3,6 +3,7 @@ alias flatten-dir="find . -mindepth 2 -type f -exec mv -t . -i '{}' +"
 
 # Shortcuts
 alias resource='source ~/.bashrc 2>/dev/null || source ~/.bash_profile'
+alias update-conf='pullc && restow-dots && resource'
 alias enw='emacs -nw'
 alias unstow='stow -D'
 alias restow='stow -R'
