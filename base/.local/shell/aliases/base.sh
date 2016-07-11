6a3eb0cf4982e88ab5c79e2521299e80a68a986d
Ethan Chan <metakirby5@gmail.com>
2016-07-10 20:36:58 -0700

    don't use qb for aniget

--- base/.local/shell/aliases/base.sh
+++ base/.local/shell/aliases/base.sh
@@ -9,7 +9,7 @@ alias restow='stow -R'
 alias tetris='bastet'
 alias ll='ls -l'
 alias la='ls -la'
-alias aniget='anistrm --program="wget -qbc"'
+alias aniget='anistrm --program="wget -c"'
 
 # Prefs
 alias less='less -R'
