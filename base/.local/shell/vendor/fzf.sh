859e59037e363685932908812b93abbbc9417226
Ethan Chan <metakirby5@gmail.com>
2016-07-22 00:54:18 -0700

    tweak options for fzf commands/binds

--- base/.local/shell/vendor/fzf.sh
+++ base/.local/shell/vendor/fzf.sh
@@ -2,12 +2,16 @@ if which fzf &>/dev/null; then
   # Show hidden files
   export FZF_DEFAULT_COMMAND='\
     ag --hidden -l 2>/dev/null || find -L . 2>/dev/null | cut -c3-'
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
   # Color scheme and file preview
   export FZF_DEFAULT_OPTS="\
     --color=fg:7,bg:-1,hl:3,fg+:7,bg+:-1,hl+:3,info:2,prompt:4,pointer:3,marker:2,spinner:1,header:5\
     --preview='(x={}; x=\"\${x/#\~/$HOME}\"; pygmentize \"\$x\" || cat \"\$x\") 2>/dev/null'\
     --bind=ctrl-o:toggle-preview"
+  export FZF_CTRL_T_OPTS="$FZF_DEFAULT_OPTS"
+  export FZF_CTRL_R_OPTS="$FZF_DEFAULT_OPTS --preview="
+  export FZF_ALT_C_OPTS="$FZF_DEFAULT_OPTS --preview="
 
   # Auto-completion
   [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
