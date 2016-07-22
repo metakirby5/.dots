aedae8020248fd9db8dac88a6c7c6686d9ba0b83
Ethan Chan <metakirby5@gmail.com>
2016-07-22 03:46:19 -0700

    why 2 /dev/nulls

--- base/.local/shell/vendor/fzf.sh
+++ base/.local/shell/vendor/fzf.sh
@@ -8,7 +8,7 @@ if which fzf &>/dev/null; then
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
   # Color scheme and file preview
-  FZF_PREVIEWER='((x={}; x="${x/#\~/$HOME}"; pygmentize "$x" 2>/dev/null || cat "$x") 2>/dev/null)'
+  FZF_PREVIEWER='((x={}; x="${x/#\~/$HOME}"; pygmentize "$x" || cat "$x") 2>/dev/null)'
   export FZF_DEFAULT_OPTS="\
     --color 'fg:7,bg:-1,hl:3,fg+:7,bg+:-1,hl+:3,info:2,prompt:4,pointer:3,marker:2,spinner:0,header:5'\
     --preview '$FZF_PREVIEWER'\
