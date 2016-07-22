8f6d76f1a9e4f8950a3e55d770448c4824f2e4f3
Ethan Chan <metakirby5@gmail.com>
2016-07-21 23:59:32 -0700

    add fzf keybinds

--- base/.local/shell/vendor/fzf.sh
+++ base/.local/shell/vendor/fzf.sh
@@ -1,8 +1,17 @@
 if which fzf &>/dev/null; then
+  # Show hidden files
   export FZF_DEFAULT_COMMAND='\
     ag --hidden -l 2>/dev/null || find -L . 2>/dev/null | cut -c3-'
+
+  # Color scheme and file preview
   export FZF_DEFAULT_OPTS="\
     --color=fg:7,bg:-1,hl:3,fg+:7,bg+:-1,hl+:3,info:2,prompt:4,pointer:3,marker:2,spinner:1,header:5\
     --preview='(x={}; x=\"\${x/#\~/$HOME}\"; pygmentize \"\$x\" || cat \"\$x\") 2>/dev/null'"
+
+  # Auto-completion
   [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+
+  # Key bindings
+  set -o vi
+  source "/usr/local/opt/fzf/shell/key-bindings.bash"
 fi
