86d25be48165b98952d78b9dd7c5e8448716b15b
Ethan Chan <metakirby5@gmail.com>
2016-07-21 23:47:07 -0700

    fzf completion

--- base/.local/shell/vendor/fzf.sh
+++ base/.local/shell/vendor/fzf.sh
@@ -4,4 +4,5 @@ if which fzf &>/dev/null; then
   export FZF_DEFAULT_OPTS="\
     --color=fg:7,bg:-1,hl:3,fg+:7,bg+:-1,hl+:3,info:2,prompt:4,pointer:3,marker:2,spinner:1,header:5\
     --preview='(x={}; x=\"\${x/#\~/$HOME}\"; pygmentize \"\$x\" || cat \"\$x\") 2>/dev/null'"
+  [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
 fi
