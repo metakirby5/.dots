9885c2cbe4c23dba8d62dc0bc2bbb8c8f1e8c9e7
Ethan Chan <metakirby5@gmail.com>
2016-07-22 00:19:41 -0700

    add toggle-preview to fzf

--- base/.local/shell/vendor/fzf.sh
+++ base/.local/shell/vendor/fzf.sh
@@ -6,7 +6,8 @@ if which fzf &>/dev/null; then
   # Color scheme and file preview
   export FZF_DEFAULT_OPTS="\
     --color=fg:7,bg:-1,hl:3,fg+:7,bg+:-1,hl+:3,info:2,prompt:4,pointer:3,marker:2,spinner:1,header:5\
-    --preview='(x={}; x=\"\${x/#\~/$HOME}\"; pygmentize \"\$x\" || cat \"\$x\") 2>/dev/null'"
+    --preview='(x={}; x=\"\${x/#\~/$HOME}\"; pygmentize \"\$x\" || cat \"\$x\") 2>/dev/null'\
+    --bind=ctrl-o:toggle-preview"
 
   # Auto-completion
   [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
