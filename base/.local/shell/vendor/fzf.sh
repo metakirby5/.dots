1d2858fcc0704d3fd7346c167e79bc1d5de4b160
Ethan Chan <metakirby5@gmail.com>
2016-07-23 10:43:44 -0700

    default command is quite a bit faser but i don't understand

--- base/.local/shell/vendor/fzf.sh
+++ base/.local/shell/vendor/fzf.sh
@@ -3,7 +3,8 @@ if which fzf &>/dev/null; then
   export FZF_DEFAULT_COMMAND='(\
     git ls-tree -r --name-only HEAD ||\
     ag --hidden -l ||\
-    find -L . | cut -c3- | tail -n+2\
+    find . -path "*/\.*" -prune -o -type f -print -o -type l -print \
+      2>/dev/null | cut -c3- \
     ) 2>/dev/null'
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
