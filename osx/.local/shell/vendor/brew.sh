7b9f14ff01a5b94735b1e03dd4896999bf08c359
Ethan Chan <metakirby5@gmail.com>
2016-07-27 03:20:59 -0700

    bash-completion, mosh, gimp

--- osx/.local/shell/vendor/brew.sh
+++ osx/.local/shell/vendor/brew.sh
@@ -1,5 +1,8 @@
 if which brew &>/dev/null; then
-  for f in $(brew --prefix)/etc/bash_completion.d/*; do
+  __brew_prefix="$(brew --prefix)"
+  for f in \
+    "$__brew_prefix"/etc/bash_completion \
+    "$__brew_prefix"/bash_completion.d/*; do
     source "$f"
   done
 fi
