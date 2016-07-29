3b6a9a94b07e4f8b274f0f584def41193d665d65
Ethan Chan <metakirby5@gmail.com>
2016-07-29 01:14:55 -0700

    add custom git cd command, also git root

--- base/.local/shell/functions/git.sh
+++ base/.local/shell/functions/git.sh
@@ -1,3 +1,26 @@
+# Custom git commands
+__mk5_git() {
+  local prog="$1"
+  shift
+  local cmd="$1"
+  shift
+
+  case "$cmd" in
+    cd)
+      cd "$(git rev-parse --show-toplevel)" "$@"
+      ;;
+    *)
+      command "$prog" "$cmd" "$@"
+      ;;
+  esac
+}
+git() {
+  __mk5_git git "$@"
+}
+hub() {
+  __mk5_git hub "$@"
+}
+
 # Publish to github-pages
 ghp-publish() {
   local cur_branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
