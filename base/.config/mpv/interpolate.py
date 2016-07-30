009f89b35d465aed78e71440bf5383a22e79f58d
Ethan Chan <metakirby5@gmail.com>
2016-07-30 07:11:37 -0700

    clean up interpolate script

--- base/.config/mpv/interpolate.py
+++ base/.config/mpv/interpolate.py
@@ -12,16 +12,20 @@ BLK = 32
 SCENE_THRESH = 32
 PRECISION = int(1e8)
 
+analyse_args = {
+    'blksize': BLK,
+    'truemotion': True,
+    'search': 3,
+}
+
 clip = video_in
 if container_fps < FPS:
     print("{}fps -> {}fps".format(container_fps, FPS))
     clip = core.std.AssumeFPS(clip, fpsnum=int(container_fps * PRECISION),
                               fpsden=PRECISION)
     sup  = core.mv.Super(clip, pel=2, hpad=BLK, vpad=BLK)
-    bvec = core.mv.Analyse(sup, truemotion=True, isb=True , chroma=True,
-                           search=3, blksize=BLK)
-    fvec = core.mv.Analyse(sup, truemotion=True, isb=False, chroma=True,
-                           search=3, blksize=BLK)
+    bvec = core.mv.Analyse(sup, isb=True , **analyse_args)
+    fvec = core.mv.Analyse(sup, isb=False, **analyse_args)
     clip = core.mv.BlockFPS(clip, sup, bvec, fvec, num=int(FPS * PRECISION),
                             den=PRECISION, mode=3, thscd2=SCENE_THRESH)
 clip.set_output()
