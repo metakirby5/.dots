c0b220e38ec7f207250e3b4446ac9d4e355761b1
Ethan Chan <metakirby5@gmail.com>
2016-08-01 00:01:15 -0700

    muh alignment

--- osx/.hammerspoon/window.lua
+++ osx/.hammerspoon/window.lua
@@ -3,10 +3,10 @@ local util = require('util')
 
 function hs.window:focusWindowIn(dir, ...)
   (util.switch(dir) {
-    [consts.NORTH] = self.focusWindowNorth,
-    [consts.SOUTH] = self.focusWindowSouth,
-    [consts.EAST] = self.focusWindowEast,
-    [consts.WEST] = self.focusWindowWest,
+    [consts.NORTH]   = self.focusWindowNorth,
+    [consts.SOUTH]   = self.focusWindowSouth,
+    [consts.EAST]    = self.focusWindowEast,
+    [consts.WEST]    = self.focusWindowWest,
     [consts.DEFAULT] = function() error('Invalid direction.') end,
   })(...)
 end
