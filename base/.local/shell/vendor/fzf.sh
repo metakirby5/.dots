2b2969e1d233b18496af23f40abb4fa79e75322b
Ethan Chan <echan@purestorage.com>
2016-08-11 14:11:40 -0700

    fzf enable preview but hide initially

--- base/.local/shell/vendor/fzf.sh
+++ base/.local/shell/vendor/fzf.sh
@@ -11,6 +11,7 @@ if which fzf &>/dev/null; then
   FZF_PREVIEWER='((x={}; x="${x/#\~/$HOME}"; pygmentize "$x" || less "$x") 2>/dev/null)'
   export FZF_DEFAULT_OPTS="\
     --color 'fg:7,bg:-1,hl:3,fg+:7,bg+:-1,hl+:3,info:2,prompt:4,pointer:3,marker:2,spinner:0,header:5'\
+    --preview '$FZF_PREVIEWER' --preview-window right:hidden
     --bind 'ctrl-l:toggle-preview,ctrl-s:toggle-sort,ctrl-o:execute[$FZF_PREVIEWER | less -R >&2]'"
   export FZF_CTRL_T_OPTS="$FZF_DEFAULT_OPTS"
   export FZF_CTRL_R_OPTS="$FZF_DEFAULT_OPTS --preview="
