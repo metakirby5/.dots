6ffdf7f760b2fc2b49b408976049c627ab0f0c6a
Ethan Chan <metakirby5@gmail.com>
2016-08-13 12:57:33 -0700

    better shell wrapper

--- base/.local/shell/functions/vim.sh
+++ base/.local/shell/functions/vim.sh
@@ -3,7 +3,7 @@
 codi() {
   vim $2 -c \
     "let g:startify_disable_at_vimenter = 1 |\
-    set buftype=nofile laststatus=0 nonu nornu |\
+    set bt=nofile ls=0 noru nonu nornu |\
     hi ColorColumn ctermbg=NONE |\
     hi VertSplit ctermbg=NONE |\
     hi NonText ctermfg=0 |\
