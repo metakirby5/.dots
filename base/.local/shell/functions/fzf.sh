429b791068644227ab707fae416861fdcf2f8960
Ethan Chan <echan@purestorage.com>
2016-08-15 16:31:31 -0700

    dan got mad about this so i'll change it

--- base/.local/shell/functions/fzf.sh
+++ base/.local/shell/functions/fzf.sh
@@ -11,7 +11,7 @@ fzc() {
 
 # fzf + ag
 zag() {
-  local c="ag --color -C 5 $@ {}"
+  local c="ag --color -C 5 $* {}"
   ag -l "$@" | fzf --multi \
     --preview "$c" \
     --bind "ctrl-o:execute-multi[$c | less -R >&2]"
