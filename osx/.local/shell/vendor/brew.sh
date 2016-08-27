a1e13b35666b29548bca3080e62d9e331dcae5c0
Ethan Chan <metakirby5@gmail.com>
2016-08-27 03:13:09 -0700

    i crapped up my completion this entire time

--- osx/.local/shell/vendor/brew.sh
+++ osx/.local/shell/vendor/brew.sh
@@ -1,5 +1,5 @@
 if which brew &>/dev/null; then
-  BREW_COMPL="$(brew --prefix)/bash_completion.d"
+  BREW_COMPL="$(brew --prefix)/etc/bash_completion.d"
   [ -d "$BREW_COMPL" ] && for f in "$BREW_COMPL"/*; do
     source "$f"
   done
