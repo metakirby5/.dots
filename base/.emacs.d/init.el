1cb331893720c000c6657ba6385b7984fb8dda3c
Ethan Chan <metakirby5@gmail.com>
2016-09-14 14:30:23 -0700

    fix pkg again, saveplace, other pkgs

--- base/.emacs.d/init.el
+++ base/.emacs.d/init.el
@@ -11,96 +11,107 @@
 (or (file-exists-p package-user-dir) (package-refresh-contents))
 
 ;; Helper to install and require
-(defun pkg (p &rest body)
+(defun pkg (p)
   (unless (require p nil t)
     (package-install p)
-    (require p))
-  body)
+    (require p)))
 
-(pkg 'mouse
-     (xterm-mouse-mode t)
-     (global-set-key [mouse-4] (lambda ()
-				 (interactive)
-				 (scroll-down 1)))
-     (global-set-key [mouse-5] (lambda ()
-				 (interactive)
-				 (scroll-up 1)))
-     (defun track-mouse (e))
-     (setq mouse-sel-mode t))
+(progn (pkg 'mouse)
+       (xterm-mouse-mode t)
+       (global-set-key [mouse-4] (lambda ()
+				   (interactive)
+				   (scroll-down 1)))
+       (global-set-key [mouse-5] (lambda ()
+				   (interactive)
+				   (scroll-up 1)))
+       (defun track-mouse (e))
+       (setq mouse-sel-mode t))
 
-(pkg 'evil
-     (evil-mode 1)
+(progn (pkg 'saveplace)
+       (setq-default save-place t))
 
-     ;; Keep Vim ctrl-u
-     (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
+(progn (pkg 'evil)
+       (evil-mode 1)
 
-     ;; Swap ; and :
-     (define-key evil-normal-state-map (kbd ";") 'evil-ex)
-     (define-key evil-normal-state-map (kbd ":") 'evil-repeat-find-char)
+       ;; Keep Vim ctrl-u
+       (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
 
-     ;; Backtab = jump backwards
-     (define-key evil-normal-state-map (kbd "<backtab>") 'evil-jump-backward)
+       ;; Swap ; and :
+       (define-key evil-normal-state-map (kbd ";") 'evil-ex)
+       (define-key evil-normal-state-map (kbd ":") 'evil-repeat-find-char)
 
-     ;; Preserve visual mode
-     (defun evil-shift-left-visual ()
-       (interactive)
-       (evil-shift-left (region-beginning) (region-end))
-       (evil-normal-state)
-       (evil-visual-restore))
+       ;; Backtab = jump backwards
+       (define-key evil-normal-state-map (kbd "<backtab>") 'evil-jump-backward)
 
-     (defun evil-shift-right-visual ()
-       (interactive)
-       (evil-shift-right (region-beginning) (region-end))
-       (evil-normal-state)
-       (evil-visual-restore))
+       ;; Preserve visual mode
+       (defun evil-shift-left-visual ()
+	 (interactive)
+	 (evil-shift-left (region-beginning) (region-end))
+	 (evil-normal-state)
+	 (evil-visual-restore))
 
-     (define-key evil-visual-state-map (kbd ">") 'evil-shift-right-visual)
-     (define-key evil-visual-state-map (kbd "<") 'evil-shift-left-visual))
+       (defun evil-shift-right-visual ()
+	 (interactive)
+	 (evil-shift-right (region-beginning) (region-end))
+	 (evil-normal-state)
+	 (evil-visual-restore))
 
-(pkg 'evil-leader
-     (global-evil-leader-mode)
-     (evil-leader/set-leader "<SPC>"))
+       (define-key evil-visual-state-map (kbd ">") 'evil-shift-right-visual)
+       (define-key evil-visual-state-map (kbd "<") 'evil-shift-left-visual)
 
-(pkg 'helm
-     (pkg 'helm-config)
-     (helm-mode 1)
+       (progn (pkg 'evil-leader)
+	      (global-evil-leader-mode)
+	      (evil-leader/set-leader "<SPC>"))
 
-     ;; Replace keybinds with helm equivalents
-     (global-set-key (kbd "M-x") 'helm-M-x)
-     (global-set-key (kbd "C-x r b") 'helm-filtered-bookmarks)
-     (global-set-key (kbd "C-x C-f") 'helm-find-files)
+       (progn (pkg 'evil-surround)
+	      (global-evil-surround-mode 1))
 
-     ;; Fuzzy all the things
-     (setq
-      helm-mode-fuzzy-match t
-      helm-completion-in-region-fuzzy-match t
-      helm-recentf-fuzzy-match t
-      helm-buffers-fuzzy-matching t
-      helm-recentf-fuzzy-match t
-      helm-buffers-fuzzy-matching t
-      helm-locate-fuzzy-match t
-      helm-M-x-fuzzy-match t
-      helm-semantic-fuzzy-match t
-      helm-imenu-fuzzy-match t
-      helm-apropos-fuzzy-match t
-      helm-lisp-fuzzy-completion t))
+       (progn (pkg 'evil-mc)
+	      (global-evil-mc-mode 1)))
 
-(pkg 'auto-complete
-     (setq ac-delay 0
-	   ac-quick-help-delay 0
-	   ac-auto-show-menu t)
-     (global-set-key (kbd "<backtab>") 'ac-previous)
-     (ac-config-default))
+(progn (pkg 'helm)
+       (progn (pkg 'helm-config)
+	      (helm-mode 1))
 
-(pkg 'smart-mode-line
-     (setq
-      sml/no-confirm-load-theme t
-      sml/theme 'respectful)
-     (sml/setup))
+       ;; Replace keybinds with helm equivalents
+       (global-set-key (kbd "M-x") 'helm-M-x)
+       (global-set-key (kbd "C-x r b") 'helm-filtered-bookmarks)
+       (global-set-key (kbd "C-x C-f") 'helm-find-files)
 
-(pkg 'git-gutter
-     (global-git-gutter-mode t)
-     (git-gutter:linum-setup))
+       ;; Fuzzy all the things
+       (setq
+	helm-mode-fuzzy-match t
+	helm-completion-in-region-fuzzy-match t
+	helm-recentf-fuzzy-match t
+	helm-buffers-fuzzy-matching t
+	helm-recentf-fuzzy-match t
+	helm-buffers-fuzzy-matching t
+	helm-locate-fuzzy-match t
+	helm-M-x-fuzzy-match t
+	helm-semantic-fuzzy-match t
+	helm-imenu-fuzzy-match t
+	helm-apropos-fuzzy-match t
+	helm-lisp-fuzzy-completion t))
+
+(progn (pkg 'auto-complete)
+       (setq ac-delay 1
+	     ac-quick-help-delay 1
+	     ac-auto-show-menu t)
+       (global-set-key (kbd "<backtab>") 'ac-previous)
+       (ac-config-default))
+
+(progn (pkg 'smart-mode-line)
+       (setq
+	sml/no-confirm-load-theme t
+	sml/theme 'respectful)
+       (sml/setup))
+
+(progn (pkg 'git-gutter)
+       (global-git-gutter-mode t)
+       (git-gutter:linum-setup))
+
+(progn (pkg 'rainbow-delimiters)
+       (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode))
 
 ;; General
 (menu-bar-mode -1) ;; Disable toolbar
@@ -112,7 +123,8 @@
 ;; Settings
 (let
     ((backupdir (concat user-emacs-directory "backups/"))
-     (undodir (concat user-emacs-directory "undo/")))
+     (undodir (concat user-emacs-directory "undo/"))
+     (saveplacefile (concat user-emacs-directory "places")))
 
   ;; Make required directories
   (mapc
@@ -133,6 +145,9 @@
    ;; Whitespace
    tab-width 2
    indent-tabs-mode nil
+
+   ;; Return to last edit position
+   save-place-file saveplacefile
 
    ;; Persistent undo
    undo-tree-auto-save-history t
