cf6ab09dcdc3446823223891b818edf5bec3e73b
Ethan Chan <metakirby5@gmail.com>
2016-09-14 05:21:01 -0700

    more emacs

--- base/.emacs.d/init.el
+++ base/.emacs.d/init.el
@@ -1,16 +1,34 @@
 ; Packages
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-(defun pkg (p)
-  (unless (require p nil t)
-    (when (not package-archive-contents)
-      (package-refresh-contents))
-    (package-install p)
-    (require p)))
+(require 'package) ; {{{
+  ; Define repos
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+  ; Activate packages
+  (package-initialize)
+
+  ; Fetch packages
+  (or (file-exists-p package-user-dir) (package-refresh-contents))
+
+  ; Helper to install and require
+  (defun pkg (p)
+    (unless (require p nil t)
+      (package-install p)
+      (require p)))
+; }}}
+
+(pkg 'mouse) ; {{{
+  (xterm-mouse-mode t)
+  (global-set-key [mouse-4] (lambda ()
+                              (interactive)
+                              (scroll-down 1)))
+  (global-set-key [mouse-5] (lambda ()
+                              (interactive)
+                              (scroll-up 1)))
+  (defun track-mouse (e))
+  (setq mouse-sel-mode t)
+; }}}
 
 (pkg 'evil) ; {{{
-  ; Start in Evil mode
   (evil-mode 1)
 
   ; Swap ; and :
@@ -26,11 +44,84 @@
 (pkg 'helm) ; {{{
   (pkg 'helm-config)
   (helm-mode 1)
+
+  ; Replace keybinds with helm equivalents
+  (global-set-key (kbd "M-x") 'helm-M-x)
+  (global-set-key (kbd "C-x r b") 'helm-filtered-bookmarks)
+  (global-set-key (kbd "C-x C-f") 'helm-find-files)
+
+  ; Fuzzy all the things
+  (setq
+    helm-mode-fuzzy-match t
+    helm-completion-in-region-fuzzy-match t
+    helm-recentf-fuzzy-match t
+    helm-buffers-fuzzy-matching t
+    helm-recentf-fuzzy-match t
+    helm-buffers-fuzzy-matching t
+    helm-locate-fuzzy-match t
+    helm-M-x-fuzzy-match t
+    helm-semantic-fuzzy-match t
+    helm-imenu-fuzzy-match t
+    helm-apropos-fuzzy-match t
+    helm-lisp-fuzzy-completion t)
 ; }}}
 
-; General {{{
-  (menu-bar-mode -1) ; Disable toolbar
-  (setq inhibit-startup-screen t) ; Disable splash screen
-  (setq tab-width 2) ; Tab = 2 spaces
-  (setq indent-tabs-mode nil) ; No tab indentation
+(pkg 'auto-complete) ; {{{
+  (ac-config-default)
 ; }}}
+
+(pkg 'smart-mode-line) ; {{{
+  (setq sml/no-confirm-load-theme t)
+  (sml/setup)
+; }}}
+
+; General
+(menu-bar-mode -1) ; Disable toolbar
+(global-linum-mode t) ; Line numbers
+
+; Aliases
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+; Settings
+(let
+  ((backupdir (concat user-emacs-directory "backups/")))
+
+  ; Make required directories
+  (mapc
+    (lambda (dir)
+      (unless (file-exists-p dir)
+        (make-directory dir)))
+    (list backupdir))
+  (setq
+    ; Appearance
+    inhibit-startup-screen t
+    scroll-step 1
+    scroll-margin 5
+
+    ; Whitespace
+    tab-width 2
+    indent-tabs-mode nil
+
+    ; Backups
+    backup-directory-alist `((".*" . ,backupdir))
+    auto-save-file-name-transforms `((".*" ,backupdir t))
+    backup-by-copying t))
+
+; Create parent directory if it doesn't exist
+(defadvice
+  find-file
+  (before make-directory-maybe (filename &optional wildcards) activate)
+  "Create parent directory if not exists while visiting file."
+  (unless (file-exists-p filename)
+    (let ((dir (file-name-directory filename)))
+      (unless (file-exists-p dir)
+        (make-directory dir)))))
+
+; Right-aligned linum
+(defadvice
+  linum-update-window
+  (around linum-dynamic activate)
+  (let*
+    ((w (length (number-to-string (count-lines (point-min) (point-max)))))
+     (linum-format (concat "%" (number-to-string w) "d ")))
+    ad-do-it))
