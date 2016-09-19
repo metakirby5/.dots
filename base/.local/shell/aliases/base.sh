b27e55e668a130c37b733aade9c8056cb9b0ebff
Ethan Chan <metakirby5@gmail.com>
2016-09-18 21:11:58 -0700

    http-server -> python alias

--- base/.local/shell/aliases/base.sh
+++ base/.local/shell/aliases/base.sh
@@ -7,6 +7,7 @@ alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.ar
 alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
 alias ip-public="dig +short myip.opendns.com @resolver1.opendns.com"
 alias ip-local="ipconfig getifaddr en0"
+alias serve="python -m SimpleHTTPServer"
 for method in GET POST PUT DELETE; do
 	alias "$method"="curl -X $method"
 done
