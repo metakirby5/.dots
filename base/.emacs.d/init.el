00ee2e1902b9fed9238fa4a69e2d00da8a5f6b10
Ethan Chan <metakirby5@gmail.com>
2016-10-04 22:17:01 -0700

    yasnippet???

--- base/.emacs.d/init.el
+++ base/.emacs.d/init.el
@@ -223,6 +223,29 @@ BINDINGS: extra keybindings."
         '(company-scrollbar-bg
           ((t (:background "brightwhite"))))))
 
+(progn (pkg 'yasnippet)
+       (yas-global-mode 1)
+
+       ;; TODO make this work
+       ;; Add yasnippet support for all company backends
+       ;; https://github.com/syl20bnr/spacemacs/pull/179
+       (defvar company-mode/enable-yas t
+         "Enable yasnippet for all backends.")
+
+       (defun company-mode/backend-with-yas (backend)
+         (if (or (not company-mode/enable-yas)
+                 (and (listp backend)
+                      (member 'company-yasnippet backend)))
+             backend
+           (append (if (consp backend)
+                       backend
+                     (list backend))
+                   '(:with company-yasnippet))))
+
+       (setq company-backends
+             (mapcar #'company-mode/backend-with-yas
+                     company-backends)))
+
 (progn (pkg 'flycheck)
        (global-flycheck-mode))
 
