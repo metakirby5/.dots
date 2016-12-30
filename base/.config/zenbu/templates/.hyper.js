a2db14ce96a3007121dfca86eef94a733d78e047
Ethan Chan <metakirby5@gmail.com>
2016-12-30 00:49:56 -0800

    altismeta

--- base/.config/zenbu/templates/.hyper.js
+++ base/.config/zenbu/templates/.hyper.js
@@ -1,34 +1,16 @@
 module.exports = {
   config: {
-    // default font size in pixels for all tabs
     fontSize: 14,
-
-    // font family with optional fallbacks
     fontFamily: 'Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
 
-    // terminal cursor background color (hex)
-    cursorColor: '#eeeeea',
-
-    // color of the text
-    foregroundColor: '#eeeeea',
-
-    // terminal background color
-    backgroundColor: '#1b1b1b',
-
-    // border color (window, tabs)
-    borderColor: '#333',
-
-    // custom css to embed in the main window
     css: '',
-
-    // custom padding (css format, i.e.: `top right bottom left`)
     termCSS: '',
-
-    // custom padding
     padding: '15px',
 
-    // some color overrides. see http://bit.ly/29k1iU2 for
-    // the full list
+    cursorColor: '#eeeeea',
+    foregroundColor: '#eeeeea',
+    backgroundColor: '#1b1b1b',
+    borderColor: '#333',
     colors: [
       '#343935',
       '#cc4061',
@@ -46,19 +28,15 @@ module.exports = {
       '#ca9dee',
       '#3deffa',
       '#fbfaf7'
-    ]
+    ],
+
+    bell: false,
+
+    modifierKeys: {
+      altIsMeta: true,
+    },
   },
 
-  // a list of plugins to fetch and install from npm
-  // format: [@org/]project[#version]
-  // examples:
-  //   `hypersolar`
-  //   `@company/project`
-  //   `project#1.0.1`
   plugins: ['hyper-vibrant'],
-
-  // in development, you can create a directory under
-  // `~/.hyperterm_plugins/local/` and include it here
-  // to load it and avoid it being `npm install`ed
   localPlugins: []
 };
