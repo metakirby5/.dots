d8e41fb799af2c9df1c90a3a7191083a601584e6
Ethan Chan <metakirby5@gmail.com>
2017-01-19 15:53:29 -0800

    add iterm template and filter

--- base/.config/zenbu/filters.py
+++ base/.config/zenbu/filters.py
@@ -4,16 +4,23 @@ import codecs
 # Hex to X
 
 def nohash(hex):
-  return hex.lstrip('#')
+    return hex.lstrip('#')
+
 
 def to_rgb(hex):
-  return struct.unpack('BBB', codecs.decode(nohash(hex), 'hex'))
+    return struct.unpack('BBB', codecs.decode(nohash(hex), 'hex'))
+
 
 def to_chrome(hex):
-  return '[{}]'.format(', '.join(map(str, to_rgb(hex))))
+    return '[{}]'.format(', '.join(map(str, to_rgb(hex))))
+
 
 # alpha: 0 - 255
 def to_apple(hex, alpha=0):
-  return '{{{}}}'.format(', '.join(map(
-    lambda x: str(x * 257), to_rgb(hex) + (alpha,)
-  )))
+    return '{{{}}}'.format(', '.join(map(
+        lambda x: str(x * 257), to_rgb(hex) + (alpha,)
+    )))
+
+# [r, g, b]
+def to_iterm(hex):
+    return [x / 255.0 for x in to_rgb(hex)]
