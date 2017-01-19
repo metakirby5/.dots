126e7a4df5c282e61fa5e68884d815b550481b04
Ethan Chan <metakirby5@gmail.com>
2017-01-19 15:25:19 -0800

    winter.yaml, update chrome theme build scripts

--- base/.config/zenbu/templates/.local/zenbu/chrome_theme/gen_img.sh
+++ base/.config/zenbu/templates/.local/zenbu/chrome_theme/gen_img.sh
@@ -3,6 +3,6 @@ images=~/.local/zenbu/chrome_theme/images
 
 mkdir -p "$images"
 convert -size 30x256 xc:{{ n_primary }} "$images/theme_frame.png"
-convert -size 30x256 xc:{{ n_secondary }} "$images/theme_frame_inactive.png"
-convert -size 30x256 xc:{{ fgc }} "$images/theme_tab_background.png"
-convert -size 30x256 xc:{{ fgc }} "$images/theme_toolbar.png"
+convert -size 30x256 xc:{{ b_primary }} "$images/theme_frame_inactive.png"
+convert -size 30x256 xc:{{ n_white }} "$images/theme_tab_background.png"
+convert -size 30x256 xc:{{ b_white }} "$images/theme_toolbar.png"
