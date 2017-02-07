190afc0c8e4ba7effb3c82e8ed78dd43bd2bd460
Ethan Chan <metakirby5@gmail.com>
2017-02-06 16:44:19 -0800

    gpg-agent stuff

---
+++ base/.local/shell/vendor/gpg.sh
@@ -0,0 +1,8 @@
+GPG_AGENT_INFO_FILE=~/.gnupg/agent-info
+mkdir -p "${GPG_AGENT_INFO_FILE%/*}"
+[ -f "$GPG_AGENT_INFO_FILE" ] && source "$GPG_AGENT_INFO_FILE"
+if [ -S "${GPG_AGENT_INFO%%:*}" ]; then
+  export GPG_AGENT_INFO
+else
+  eval $(gpg-agent --daemon --write-env-file "$GPG_AGENT_INFO_FILE")
+fi
