44f76b99d8f5ee098d42fad64087c3efb52d5598
Ethan Chan <metakirby5@gmail.com>
2017-11-26 17:55:36 -0800

    python workflow configuration

--- base/.local/shell/aliases/base.sh
+++ base/.local/shell/aliases/base.sh
@@ -21,8 +21,8 @@ alias tetris='bastet'
 alias findr='find -L . ! -type d -regex'
 alias HEAD='git rev-parse --abbrev-ref HEAD'
 alias aniget='anistrm --program="wget -c"'
-alias pypush='python setup.py sdist bdist_wheel upload -r live'
-alias pypush3='python3 setup.py sdist bdist_wheel upload -r live'
+alias pypush='python setup.py sdist bdist_wheel upload -r'
+alias pypush3='python3 setup.py sdist bdist_wheel upload -r'
 alias all="tr '\n' '\0' | xargs -0"
 alias map="all -n 1"
 alias fst="awk '{print\$1}'"
