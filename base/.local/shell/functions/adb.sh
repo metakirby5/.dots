8d81922a7f0cd16d8e7981a5b3791f0a4aed9ab7
Ethan Chan <echan@nianticlabs.com>
2018-04-28 19:17:56 -0700

    Move apk-reinstall to scripts.

--- base/.local/shell/functions/adb.sh
+++ base/.local/shell/functions/adb.sh
@@ -11,19 +11,7 @@ adb() {
       command adbf "$@"
       ;;
     reinstall)
-      echo "Attempting install..."
-      local PATTERN='.*\[INSTALL_FAILED_ALREADY_EXISTS: Attempt to re-install \([^\s]*\) without first uninstalling.\].*'
-      local out="$(command adb install "$@" 2>&1)"
-      local pkg="$(sed -n "s/$PATTERN/\1/p" <<< "$out")"
-      if [ "$pkg" ]; then
-        echo "Package detected: $pkg"
-        echo "Uninstalling..."
-        command adb uninstall "$pkg"
-        echo "Reinstalling..."
-        command adb install "$@"
-      else
-        echo "$out"
-      fi
+      command apk-reinstall "$@"
       ;;
     *)
       command adb "$cmd" "$@"
