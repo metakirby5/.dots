bac624cedacfd7430691ce660eb48aa0aad373f4
Ethan Chan <metakirby5@gmail.com>
2018-07-29 17:30:12 -0700

    Update JAVA_HOME.

--- base/.local/shell/vendor/java.sh
+++ base/.local/shell/vendor/java.sh
@@ -1,4 +1,4 @@
 if which java &>/dev/null; then
   export CLASSPATH='*':'.' # Use current directory for default Java classpath
-  export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+  export JAVA_HOME="$(/usr/libexec/java_home)"
 fi
