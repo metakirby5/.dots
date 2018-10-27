8078c060dbd32e0a90b16578e47d29e413764e8a
Ethan Chan <echan@nianticlabs.com>
2018-10-26 21:58:53 -0700

    kubernetes: Add barebones argument checking to subcommands.

--- base/.local/shell/functions/kubernetes.sh
+++ base/.local/shell/functions/kubernetes.sh
@@ -8,16 +8,19 @@ kubectl() {
       ;;
 
     pod)
+      [ -z "$1" ] && echo 'Arg required.' && return 1
       command kubectl get pod | awk "/^$1-[a-z0-9]{5}/ {print \$1}"
       ;;
 
     node)
+      [ -z "$1" ] && echo 'Arg required.' && return 1
       command kubectl describe pod "$(kubectl pod "$1")" |
         awk '/^Node:/ {print $2}' |
         cut -d/ -f1
       ;;
 
     pod-do)
+      [ -z "$1" ] && echo 'Arg required.' && return 1
       local pod="$(kubectl pod "$1")"
       shift
 
@@ -25,10 +28,12 @@ kubectl() {
       ;;
 
     threads)
+      [ -z "$1" ] && echo 'Arg required.' && return 1
       kubectl pod-do "$1" jstack 1
       ;;
 
     classes)
+      [ -z "$1" ] && echo 'Arg required.' && return 1
       kubectl pod-do "$1" jcmd 1 GC.class_histogram
       ;;
 
