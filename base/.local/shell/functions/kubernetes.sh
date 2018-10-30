402f55cef9774a26f1c315f6a0e7587633644e43
Ethan Chan <echan@nianticlabs.com>
2018-10-30 16:56:47 -0700

    kubernetes: add ctx, fzf-ify most commands.

--- base/.local/shell/functions/kubernetes.sh
+++ base/.local/shell/functions/kubernetes.sh
@@ -3,38 +3,66 @@ kubectl() {
   shift
 
   case "$cmd" in
+    # Switch contexts with fzf.
+    ctx)
+      local chosen="$(
+        command kubectl config get-contexts |
+          rev | awk '{$1 = ""; $2 = ""} NR > 1' | rev |
+          awk 'NF == 1 {print "\t" $1} NF == 2 {print $1 "\t" $2}' |
+          fzf | awk '{print $NF}'
+      )"
+
+      [ "$chosen" ] && 
+        command kubectl config set current-context "$chosen" ||
+        true
+      ;;
+
+    # Describe pod.
     desc)
       command kubectl describe pod "$@"
       ;;
 
-    pod)
-      [ -z "$1" ] && echo 'Arg required.' && return 1
-      command kubectl get pod | awk "/^$1-[a-z0-9]{5}/ {print \$1}"
+    # List pods with fzf.
+    pods)
+      command kubectl get pods "$@" | fzf --header-lines=1 | awk '{print $1}'
       ;;
 
+    # Get full pod name.
+    pod)
+      [ -z "$1" ] &&
+        kubectl pods ||
+        command kubectl get pod | awk "/^$1-[a-z0-9]{5}/ {print \$1}"
+      ;;
+
+    # Get node name.
     node)
-      [ -z "$1" ] && echo 'Arg required.' && return 1
       command kubectl describe pod "$(kubectl pod "$1")" |
         awk '/^Node:/ {print $2}' |
         cut -d/ -f1
       ;;
 
+    # exec on a pod.
     pod-do)
-      [ -z "$1" ] && echo 'Arg required.' && return 1
       local pod="$(kubectl pod "$1")"
       shift
 
       command kubectl exec "$pod" -- "$@"
       ;;
 
+    # Thread dump a pod.
     threads)
-      [ -z "$1" ] && echo 'Arg required.' && return 1
       kubectl pod-do "$1" jstack 1
       ;;
 
+    # Get class histogram of a pod.
     classes)
-      [ -z "$1" ] && echo 'Arg required.' && return 1
       kubectl pod-do "$1" jcmd 1 GC.class_histogram
+      ;;
+
+    # Heap dump a pod.
+    heap)
+      kubectl pod-do "$1" jmap -dump:live,format=b,file=heap.bin 1
+      kubectl pod-do "$1" jhat heap.bin
       ;;
 
     *)
