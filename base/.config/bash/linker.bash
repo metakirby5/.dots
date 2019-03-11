25a27a7def739da7dd1e4fc6f0736814bdc7acac
Ethan Chan <metakirby5@gmail.com>
2019-03-11 02:41:51 -0700

    Optimize linker.

--- base/.config/bash/linker.bash
+++ base/.config/bash/linker.bash
@@ -3,11 +3,8 @@
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Source configs.
-for f in \
-  $(find -L "$dir/configs/" -type f | sort)\
-  ~/.bash_completion.d/*\
-  ; do
-  [ -f "$f" ] && source "$f"
+for f in $(find -L "$dir/configs/" -type f | sort); do
+  source "$f"
 done
 
 # Reset the exit code.
