6db30f71819765c85129621b86e23ef99fc752e8
Ethan Chan <metakirby5@gmail.com>
2019-03-11 03:32:18 -0700

    Cache fasd init.

--- base/.config/bash/configs/fasd.bash
+++ base/.config/bash/configs/fasd.bash
@@ -1,5 +1,10 @@
 if which fasd &>/dev/null; then
-  eval "$(fasd --init posix-alias posix-hook)"
+  fasd_cache="$HOME/.fasd-init"
+  [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ] &&
+    fasd --init posix-alias posix-hook >| "$fasd_cache"
+  source "$fasd_cache"
+  unset fasd_cache
+
   alias \
     v='f -e vim' \
     o='a -e open'
