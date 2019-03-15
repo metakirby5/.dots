d2da2dc2e023144e33c0f162b6a9bd54ba3957c0
Ethan Chan <echan@nianticlabs.com>
2019-03-15 14:14:04 -0700

    Fix fasd hook.

--- base/.config/bash/configs/fasd.bash
+++ base/.config/bash/configs/fasd.bash
@@ -2,7 +2,7 @@ fasd_path="$(command -v fasd)"
 if [ "$fasd_path" ]; then
   fasd_cache="$HOME/.fasd-init"
   [ "$fasd_path" -nt "$fasd_cache" -o ! -s "$fasd_cache" ] &&
-    fasd --init posix-alias posix-hook >| "$fasd_cache"
+    fasd --init posix-alias bash-hook >| "$fasd_cache"
   source "$fasd_cache"
   unset fasd_cache
 
