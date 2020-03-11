92197ec2c43a1f445bae8fe662583f174775d88d
Ethan Chan <metakirby5@gmail.com>
2020-03-11 16:18:14 +0900

    bash: Silence java_home warnings.

--- base/.config/bash/configs/java.bash
+++ base/.config/bash/configs/java.bash
@@ -1,7 +1,7 @@
 if command -v java &>/dev/null; then
   export \
     CLASSPATH='*':'.' \
-    JAVA_HOME="$(/usr/libexec/java_home)"
+    JAVA_HOME="$(/usr/libexec/java_home 2>/dev/null)"
 
   # Compile and run a java file.
   javar() {
