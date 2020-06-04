7b9f7a09b3a21e4dad6cd02d35c8435ecb8d646a
Ethan Chan <echan@nianticlabs.com>
2020-06-04 15:15:43 +0900

    bitbar: Remove weather.

--- osx/.config/bitbar/3calclock.1s.sh
+++ osx/.config/bitbar/3calclock.1s.sh
@@ -1,22 +1,5 @@
 #!/usr/bin/env bash
 
-vpad() {
-  tmp="$(mktemp)"
-  cat >"$tmp"
-
-  len="$(wc -l < "$tmp")"
-  if [ $len -gt 0 ]; then
-    pad="$(echo $(($1 - $len)))"
-    above="$(echo $(($pad / 2)))"
-    below="$(echo $((($pad + 1) / 2)))"
-    [ $above -gt 0 ] && yes ' ' | head -n $above
-    cat "$tmp"
-    [ $below -gt 0 ] && yes ' ' | head -n $below
-  fi
-
-  rm -f "$tmp"
-}
-
 pad_cal() {
   cat - <(echo ' ') |
     sed 's/.//g' |
@@ -43,10 +26,6 @@ cal="$(paste -d ' '\
   <(cal $nm $ny | pad_cal) |
   awk 'NF {print $0 "|trim=false font=Menlo size=12"}' | cut -c2-)"
 
-wtr="$(curl -sf 'wttr.in?m' | head -n7 | awk 'NF' |
-  vpad "$(wc -l <<< "$cal")" |
-  awk 'length {print $0 "|alternate=true trim=false font=Menlo size=12"}')"
-
 LANG='ja_JP.UTF-8' date '+%a %-m/%-d %-H:%M'
 echo '---'
-paste -d '\n' <(echo "$cal") <(echo "$wtr")
+echo "$cal"
