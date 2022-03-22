528021e0e2129686f0ffb23d84aa99891690b4fe
Ethan Chan <metakirby5@gmail.com>
2022-03-22 10:26:03 +0900

    Source CLOUD_SDK completions.

--- osx/.config/bash/configs/gcloud.bash
+++ osx/.config/bash/configs/gcloud.bash
@@ -1,1 +1,6 @@
 export CLOUDSDK_PYTHON="$HOMEBREW_PREFIX/opt/python/libexec/bin/python"
+CLOUD_SDK="$HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk"
+for f in "$CLOUD_SDK"/*.bash.inc; do
+  source "$f"
+done
+unset CLOUD_SDK
