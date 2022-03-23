6a6ffbc7c9953d677a0c01aff6fb9240920c3573
Ethan Chan <metakirby5@gmail.com>
2022-03-23 23:09:41 +0900

    Check for CLOUD_SDK directory before sourcing.

--- osx/.config/bash/configs/gcloud.bash
+++ osx/.config/bash/configs/gcloud.bash
@@ -1,6 +1,8 @@
 export CLOUDSDK_PYTHON="$HOMEBREW_PREFIX/opt/python/libexec/bin/python"
 CLOUD_SDK="$HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk"
-for f in "$CLOUD_SDK"/*.bash.inc; do
-  source "$f"
-done
+if [ -d "$CLOUD_SDK" ]; then
+  for f in "$CLOUD_SDK"/*.bash.inc; do
+    source "$f"
+  done
+fi
 unset CLOUD_SDK
